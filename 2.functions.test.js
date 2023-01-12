98fa030bb75372624a8450598ebf9ce2bc91e13a
Rémi Doolaeghe <remi.doolaeghe@gmail.com>
2023-01-12 17:58:10 +0100

    fix

--- 2.functions.test.js
+++ 2.functions.test.js
@@ -1,51 +1,58 @@
-function fillArrayFromTo(start, end) {
-  return Array(end - start + 1).fill(null).map((item, i) => start + i)
-}
+describe('Sums', () => {
 
-function sumNumbers(first, last) {
-  // TODO
-}
 
-it('should compute the sum from 1 to 10', () => {
-  const expected = 'TODO'
-  expect(sumNumbers(1, 10)).toEqual(expected)
+  function fillArrayFromTo (start, end) {
+    return Array(end - start + 1).fill(null).map((item, i) => start + i)
+  }
+
+  function sumNumbers (first, last) {
+    // TODO
+  }
+
+  it('should compute the sum from 1 to 10', () => {
+    const expected = 'TODO'
+    expect(sumNumbers(1, 10)).toEqual(expected)
+  })
+
+  it('should compute the sum from 10 to 20', () => {
+    const expected = 'TODO'
+    expect(sumNumbers(10, 20)).toEqual(expected)
+  })
+
+  it('should compute the sum from 1 to 1', () => {
+    const expected = 'TODO'
+    expect(sumNumbers(1, 1)).toEqual(expected)
+  })
+
+  it('should compute the sum from 10 to 1', () => {
+    const expected = 'TODO'
+    expect(sumNumbers(10, 1)).toEqual(expected)
+  })
 })
 
-it('should compute the sum from 10 to 20', () => {
-  const expected = 'TODO'
-  expect(sumNumbers(10, 20)).toEqual(expected)
-})
 
-it('should compute the sum from 1 to 1', () => {
-  const expected = 'TODO'
-  expect(sumNumbers(1, 1)).toEqual(expected)
-})
+describe('fizzbuzz', () => {
+  function fizzBuzz (param) {
+    // TODO
+  }
 
-it('should compute the sum from 10 to 1', () => {
-  const expected = 'TODO'
-  expect(sumNumbers(10, 1)).toEqual(expected)
-})
+  it('should return an empty result for 1', () => {
+    const expected = 'TODO'
+    expect(fizzBuzz(1)).toEqual('')
+  })
 
-function fizzBuzz(param) {
-  // TODO
-}
+  it('should return fizz for 3', () => {
+    const expected = 'TODO'
+    expect(fizzBuzz(3)).toEqual('')
+  })
 
-it('should return an empty result for 1', () => {
-  const expected = 'TODO'
-  expect(fizzBuzz(1)).toEqual('')
-})
+  it('should return buzz for 5', () => {
+    const expected = 'TODO'
+    expect(fizzBuzz(1)).toEqual('')
+  })
 
-it('should return fizz for 3', () => {
-  const expected = 'TODO'
-  expect(fizzBuzz(3)).toEqual('')
-})
-
-it('should return buzz for 5', () => {
-  const expected = 'TODO'
-  expect(fizzBuzz(1)).toEqual('')
-})
-
-it('should return fizzbuzz for 15', () => {
-  const expected = 'TODO'
-  expect(fizzBuzz(15)).toEqual('')
+  it('should return fizzbuzz for 15', () => {
+    const expected = 'TODO'
+    expect(fizzBuzz(15)).toEqual('')
+  })
 })
--- 3.collections.test.js
+++ 3.collections.test.js
@@ -16,49 +16,56 @@ const data = [
   }
 ]
 
-function getNames() {
-  // TODO
-}
 
-it('should return an array of all names', () => {
-  const expected = 'TODO'
-  expect(getNames()).toEqual(expected)
+describe('Names', () => {
+  function getNames () {
+    // TODO
+  }
+
+  it('should return an array of all names', () => {
+    const expected = 'TODO'
+    expect(getNames()).toEqual(expected)
+  })
 })
 
-function getNamesUnder(age) {
-  // TODO
-}
+describe('Names under', () => {
+  function getNamesUnder (age) {
+    // TODO
+  }
 
-it('should return Tobby, Rex and Felix under age 12', () => {
-  const expected = 'TODO'
-  expect(getNamesUnder(12)).toEqual(expected)
+  it('should return Tobby, Rex and Felix under age 12', () => {
+    const expected = 'TODO'
+    expect(getNamesUnder(12)).toEqual(expected)
+  })
+
+  it('should return Rex and Felix under age 8', () => {
+    const expected = 'TODO'
+    expect(getNamesUnder(8)).toEqual(expected)
+  })
+
+  it('should return Tobby, Rex and Felix under age 7', () => {
+    const expected = 'TODO'
+    expect(getNamesUnder(7)).toEqual(expected)
+  })
+
+  it('should return Felix under age 1', () => {
+    const expected = 'TODO'
+    expect(getNamesUnder(1)).toEqual(expected)
+  })
+
+  it('should return an empty array under age 0', () => {
+    const expected = 'TODO'
+    expect(getNamesUnder(0)).toEqual(expected)
+  })
 })
 
-it('should return Rex and Felix under age 8', () => {
-  const expected = 'TODO'
-  expect(getNamesUnder(8)).toEqual(expected)
-})
+describe('Mean age', () => {
+  function getDogsMeanAge () {
+    // TODO
+  }
 
-it('should return Tobby, Rex and Felix under age 7', () => {
-  const expected = 'TODO'
-  expect(getNamesUnder(7)).toEqual(expected)
-})
-
-it('should return Felix under age 1', () => {
-  const expected = 'TODO'
-  expect(getNamesUnder(1)).toEqual(expected)
-})
-
-it('should return an empty array under age 0', () => {
-  const expected = 'TODO'
-  expect(getNamesUnder(0)).toEqual(expected)
-})
-
-function getDogsMeanAge() {
-  // TODO
-}
-
-it('should return dog names', () => {
-  const expected = 'TODO'
-  expect(getDogsMeanAge()).toEqual(expected)
+  it('should return dog names', () => {
+    const expected = 'TODO'
+    expect(getDogsMeanAge()).toEqual(expected)
+  })
 })
