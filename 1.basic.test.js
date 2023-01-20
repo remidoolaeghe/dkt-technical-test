dea09aa5984b96a1d8ba06ea5b53c9a0008d5ba9
Rémi Doolaeghe <51918193+DktRemiDoolaeghe@users.noreply.github.com>
2023-01-20 14:52:04 +0100

    Fix tests

--- 1.basic.test.js
+++ 1.basic.test.js
@@ -1,10 +1,10 @@
-it('should test true to be true', () => {
+it('should test falsy value', () => {
   const actual = 'TODO'
   expect(actual).toBeFalsy()
 })
 
 it('should concatenate string and number', () => {
-  const actual = "value" + 10
+  const actual = "10" + 10
   const expected = 'TODO'
   expect(actual).toEqual(expected)
 })
